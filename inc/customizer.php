508ee30432c2e2f11a9fda5860b17e5c7ae2420d
PraJaL55 <trivediprajal@yahoo.co.in>
2017-04-10 03:17:52 +0530

    Initial Commit :)
    _s theme

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * sds Theme Customizer
+ *
+ * @package sds
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function sds_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'sds_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function sds_customize_preview_js() {
+	wp_enqueue_script( 'sds_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'sds_customize_preview_js' );
