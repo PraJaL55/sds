b006bdf1f1035b25037720f268f73095c4433afb
Jay17231 <gajjarjay23@gmail.com>
2017-04-10 07:44:59 +0530

    Added : Header
    Header added to the new site

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<?php
+	<?php
 /**
  * The header for our theme
  *
@@ -12,15 +12,19 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Cabin" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
+	<!--
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'sds' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
@@ -39,11 +43,47 @@
 			<?php
 			endif; ?>
 		</div><!-- .site-branding -->
-
+		<!--
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sds' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
+<!--</header><!-- #masthead -->
+	
+<header id="Header">
+	<div class="row">
+		<div class="col-xs-12 col-lg-2 sds-logo">
+			<p><a href="/"><img src="http://www.satnam.com/wp-content/uploads/2015/01/1111111.png" alt="Logo" style="height: 100%;"></a></p>
+		</div>
+		<div class="col-lg-10 main-menu pull-right">
+			<ul>
+				<li class="nav-bar-items">
+					<a href="javascript:void(0)" class="reg-pointer">HOME</a>
+					<div class="fulldrop">
+					</div>
+				</li>
+				<li class="nav-bar-items">
+					<a href="javascript:void(0)" class="reg-pointer">WHO WE ARE</a>
+					<div class="fulldrop">
+					</div>
+				</li>
+				<li class="nav-bar-items">
+					<a href="javascript:void(0)" class="reg-pointer">WHAT WE DO</a>
+					<div class="fulldrop">
+					</div>
+				</li>
+				<li class="nav-bar-items">
+					<a href="javascript:void(0)" class="reg-pointer">WHAT THEY SAY</a>
+					<div class="fulldrop">
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="grad-border row">
+		<div class="col-lg-6 grad-blue"></div>
+		<div class="col-lg-6 grad-red"></div>
+	</div>
+</header>	
+	
 	<div id="content" class="site-content">
--- index.php
+++ index.php
@@ -13,7 +13,7 @@
  */
 
 get_header(); ?>
-
+<?php /*
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -28,14 +28,14 @@ get_header(); ?>
 			<?php
 			endif;
 
-			/* Start the Loop */
+			/* Start the Loop 
 			while ( have_posts() ) : the_post();
 
 				/*
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
+				 
 				get_template_part( 'template-parts/content', get_post_format() );
 
 			endwhile;
@@ -50,7 +50,10 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?php
+*/ ?>
+<?php /*
 get_sidebar();
-get_footer();
+*/ ?>
+<?php
+// get_footer();
+?>
